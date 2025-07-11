6a863853397aa8e54033c4f519803ff67efa9811
Vinicius Taliari <vinicius.taliari@ideasdesdecero.es>
2025-07-11 13:04:20 +0200

    feat: Añaiendo minijuegos para pruebas

---
+++ juegos/Soccer_Goal/scripts/project/javaScriptInEvents.js
@@ -0,0 +1,36 @@
+
+
+const scriptsInEvents = {
+
+	async Es_game_Event65_Act2(runtime, localVars)
+	{
+		
+		if(runtime.globalVars.ultima_puntuacion != runtime.globalVars.score){
+			runtime.globalVars.ultima_puntuacion = runtime.globalVars.score
+			console.log(runtime.globalVars.score)
+			function enviarDatos(data) {
+					fetch('http://localhost:8000/src/api_puntos.php', {
+						method: 'POST',
+						headers: {
+						'Content-Type': 'application/json'
+						},
+						body: JSON.stringify(data)
+					})
+					.then(response => response.text())
+					.then(resultado => {
+						console.log(resultado);                     // Comprobamos los datos recibidos por el servidor
+					})
+					.catch(error => {
+						console.error('Error:', error);
+					});
+					}
+			let data = runtime.globalVars.score
+			let dataPrepared = {
+				pt : data
+			}
+			enviarDatos(dataPrepared)
+		}
+	}
+};
+
+globalThis.C3.JavaScriptInEvents = scriptsInEvents;
