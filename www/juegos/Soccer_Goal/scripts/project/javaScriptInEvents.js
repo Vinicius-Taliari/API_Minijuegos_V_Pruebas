61e931afb2c2215c5de4411a4c0fc8ff40153c33
Vinicius Taliari <vinicius.taliari@ideasdesdecero.es>
2025-07-17 13:27:41 +0200

    style: Cambiando assets minijuegos

--- www/juegos/Soccer_Goal/scripts/project/javaScriptInEvents.js
+++ www/juegos/Soccer_Goal/scripts/project/javaScriptInEvents.js
@@ -2,33 +2,34 @@
 
 const scriptsInEvents = {
 
-	async Es_game_Event65_Act2(runtime, localVars) {
-
-		if (runtime.globalVars.ultima_puntuacion != runtime.globalVars.score) {
+	async Es_game_Event65_Act2(runtime, localVars)
+	{
+		
+		if(runtime.globalVars.ultima_puntuacion != runtime.globalVars.score){
 			runtime.globalVars.ultima_puntuacion = runtime.globalVars.score
 			console.log(runtime.globalVars.score)
 			function enviarDatos(data) {
-				fetch('http://localhost:8000/server/api_puntos.php', {
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/json'
-					},
-					body: JSON.stringify(data)
-				})
-					.then(response => response.text())
-					.then(resultado => {
-						console.log(resultado);                     // Comprobamos los datos recibidos por el servidor
-					})
-					.catch(error => {
-						console.error('Error:', error);
-					});
-			}
-			let data = runtime.globalVars.score
-			let dataPrepared = {
-				pt: data,
-				id_juego: 3, // Soccer Goal
-			}
-			enviarDatos(dataPrepared)
+							fetch('http://localhost:8000/server/api_puntos.php', {
+								method: 'POST',
+								headers: {
+								'Content-Type': 'application/json'
+								},
+								body: JSON.stringify(data)
+							})
+							.then(response => response.text())
+							.then(resultado => {
+								console.log(resultado);                     // Comprobamos los datos recibidos por el servidor
+							})
+							.catch(error => {
+								console.error('Error:', error);
+							});
+							}
+					let data = runtime.globalVars.score
+					let dataPrepared = {
+						pt : data,
+						id_juego : 3, // Soccer Goal
+					}
+					enviarDatos(dataPrepared)
 		}
 	}
 };
